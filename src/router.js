388eee7f8af748793d06a6dc4d57c9e9b2419b8b
valerioperria <valerio.perria@gmail.com>
2024-02-02 16:02:27 +0100

    feat. AboutUsPage, HomePage, router.js and routes

---
+++ src/router.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import HomePage from './pages/HomePage.vue';
+import AboutUsPage from './pages/AboutUsPage.vue';
+
+const router = createRouter({
+    history:createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomePage
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: AboutUsPage
+        },
+    ]
+});
+
+export { router };
