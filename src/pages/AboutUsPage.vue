fa5786ac246f0bd6fb4e288b9d7681f5776b8d05
valerioperria <valerio.perria@gmail.com>
2024-02-03 15:31:46 +0100

    add. pages

---
+++ src/pages/AboutUsPage.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <div class="container mt-3">
+    <h1>About our blog</h1>
+  </div>
+</template>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+import ProjectCard from '../components/ProjectCard.vue';
+
+export default {
+    data() {
+        return {
+            baseUrl: "http://127.0.0.1:8000",
+            projects: [],
+        }
+    },
+    created() {
+        axios.get(`${this.baseUrl}/api/projects`).then((resp) => {
+            this.projects = resp.data.result;
+        });
+    },
+    components: { ProjectCard }
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-3">
+            <div v-for="project in projects" class="col" :key="project.id">
+                <ProjectCard :project="project" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
