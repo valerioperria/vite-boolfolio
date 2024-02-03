174800332605f254c322dce2b07459ffb678e21f
valerioperria <valerio.perria@gmail.com>
2024-02-03 15:31:23 +0100

    add. AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          name: 'home',
+          title: 'Home'
+        },
+        {
+          name: 'about',
+          title: 'About Us'
+        },
+      ]
+    }
+  }
+};
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar bg-primary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Boolpress</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="ms-auto navbar-nav">
+          <li class="nav-item" v-for="link in links" :key="link.title">
+            <router-link :to="{name: link.name}" class="nav-link">{{ link.title }}</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped></style>
