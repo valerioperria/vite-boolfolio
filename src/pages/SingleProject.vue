30b2f5cc2c24bd7b01e14598b209a3e16f6bdfcf
valerioperria <valerio.perria@gmail.com>
2024-02-04 16:27:40 +0100

    feat. SingleProject_axios and Loading

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -1,14 +1,38 @@
 <script>
-export default {
+import axios from 'axios';
+import { store } from '../store';
 
+export default {
+    data() {
+        return {
+            store,
+            curProject: null,
+            loading: false
+        }
+    },
+    created() {
+        console.log(this.$route.params.slug);
+        this.loading = true;
+        axios.get(`${store.baseUrl}/api/projects/${this.$route.params.slug}`)
+            .then((resp) => {
+                this.curProject = resp.data.results;
+            })
+            .finally(() => {
+                this.loading = false;
+            })
+    },
 }
 </script>
 
 <template>
-<div class="container mt-4">
-    <h1>Single Project</h1>
-</div>
+    <div class="container mt-4">
+        <div v-if="loading">
+            <h3>Loading...</h3>
+        </div>
+        <div v-else>
+            <h1>{{ curProject.title }}</h1>
+        </div>
+    </div>
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
