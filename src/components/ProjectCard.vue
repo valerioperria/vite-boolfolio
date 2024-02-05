f2c8c0cf080389a3a4e8e357ad38cfa676dadf37
valerioperria <valerio.perria@gmail.com>
2024-02-05 14:04:46 +0100

    fix. SingleProject_page

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -20,12 +20,12 @@ export default {
         <div class="card-body">
             <h5>{{ project.title }}</h5>
             <h6>{{ project.content }}</h6>
-            <p>Category:
+            <!-- <p>Category:
                 {{ project.category ? project.category.name : "No category" }}</p>
             <p>Type:
                 {{ project.type ? project.type.name : "No type" }}</p>
             <p>Tecnology:
-                {{ project.tecnology ? project.tecnology.name : "No Tecnology" }}</p>
+                {{ project.tecnology ? project.tecnology.name : "No Tecnology" }}</p> -->
             <p>Comment:
                 {{ project.comment ? project.comment : "No Comment" }}</p>
 
--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -13,7 +13,7 @@ export default {
     created() {
         console.log(this.$route.params.slug);
         this.loading = true;
-        axios.get(`${store.baseUrl}/api/projects/${this.$route.params.slug}`)
+        axios.get(`${this.store.baseUrl}/api/projects/${this.$route.params.slug}`)
             .then((resp) => {
                 this.curProject = resp.data.results;
             })
@@ -31,6 +31,21 @@ export default {
         </div>
         <div v-else>
             <h1>{{ curProject.title }}</h1>
+            <h6>{{ curProject.content }}</h6>
+            <p>Category:
+                {{ curProject.category ? curProject.category.name : "No category" }}</p>
+            <p>Type:
+                {{ curProject.type ? curProject.type.name : "No type" }}</p>
+            <p>Tecnology:
+                {{ curProject.tecnology ? curProject.tecnology.name : "No Tecnology" }}</p>
+            <p>Comment:
+                {{ curProject.comment ? curProject.comment : "No Comment" }}</p>
+            <div v-if="`!${store.baseUrl}/storage/${curProject.cover_image}`">
+                <h6>No image</h6>
+                </div>
+            <div v-else>
+                <img :src="`${store.baseUrl}/storage/${curProject.cover_image}`" alt="">
+            </div>
         </div>
     </div>
 </template>
